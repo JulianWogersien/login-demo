a22aeaa8fdc3851481f6c391b84c0d9cab3d571e
Julian <julian.wogersien@gmail.com>
2024-12-31 16:04:19 +0100

    lciense

--- register.php
+++ register.php
@@ -1,3 +1,8 @@
+Copyright (C) 2024 Julian Wogersien
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3.
+
 <?php
 error_reporting(E_ALL);
 ini_set("display_errors",1);
